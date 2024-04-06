283e20e070226d02c6a85e4b9ead2eed93dc9d71
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-06 15:00:08 +0200

    ft_strcat and ft_strncat done

---
+++ ft_strcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 14:29:40 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/06 14:46:06 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcat(char *dest, const char *src)
+{
+	unsigned int	i;
+	int				len;
+
+	i = 0;
+	len = ft_strlen(dest);
+	while (src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
---
+++ ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/06 14:29:40 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/06 14:57:26 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strncat(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(dest);
+	while (i < n && src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
