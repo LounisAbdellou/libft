bdbb0dc0b4a48942ddf86c88afd51acb7a12dc98
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-06 15:53:58 +0200

    ft_strcmp and ft_strncmp done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,21 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/06 14:34:47 by labdello          #+#    #+#             */
-/*   Updated: 2024/04/06 15:36:53 by labdello         ###   ########.fr       */
+/*   Created: 2024/04/06 15:23:03 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/06 15:52:19 by labdello         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include "libft.h"
+
+int	ft_strncmp(char *s1, char *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-	return (i);
+	}
+	return (0);
 }
