7a7339b7bd848a19a5f2a35a7bc8f71aec133311
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-08 17:37:47 +0200

    ft_memmove done

---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 17:34:42 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/08 17:47:07 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*src_ptr;
+	unsigned char	*dest_ptr;
+
+	src_ptr = (unsigned char *)src;
+	dest_ptr = (unsigned char *)dest;
+	if (dest > src)
+	{
+		i = n - 1;
+		while (i > 0)
+		{
+			dest_ptr[i] = src_ptr[i];
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			dest_ptr[i] = src_ptr[i];
+			i++;
+		}
+	}
+	return (dest);
+}
