f5ca8df12a8d05f5f77226cf01e5e9f7fce67164
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2024-04-10 23:35:00 +0200

    ft_strlcat done

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: labdello <labdello@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 23:32:13 by labdello          #+#    #+#             */
+/*   Updated: 2024/04/11 23:56:13 by labdello         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlcat(char *dest, char *src, size_t n)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dest_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	if (n <= dest_len)
+		return (src_len + n);
+	while ((dest_len + i) < (n - 1) && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
